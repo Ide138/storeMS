d0dc76e87404338235ee3680da34e51aa0da3060
smg8710 <tedy13082005@gmail.com>
2023-11-07 14:35:38 +0200

    Update 7.11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import com.company.model.Product;
+import com.company.service.ProductService;
+import com.company.service.ProductServiceI;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws SQLException {
+        ProductServiceI service = new ProductService();
+        List<Product> allProducts = service.getAll();
+        for (Product p :allProducts) {
+            System.out.println(p.getName());
+        }
     }
 }
---
+++ src/com/company/model/Category.java
@@ -0,0 +1,9 @@
+package com.company.model;
+
+public class Category {
+    private  int id ;
+    private String name;
+
+    private boolean  is_active ;
+
+}
